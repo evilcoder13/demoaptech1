04c77ec90acad4184ede1f9709909a43e9e3eb81
Dao Manh Thang <thangdm1@vnpt.vn>
2019-06-13 19:47:05 +0700

    demo 1

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace duanmoi
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // abcdef
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -37,7 +37,6 @@ namespace duanmoi
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
